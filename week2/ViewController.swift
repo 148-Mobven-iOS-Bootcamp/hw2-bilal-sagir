51d2e3e24411d67a10e5ee680e2e3d94b9a3ea8c
bilal-sagir <sagir.bilal@icloud.com>
2021-12-24 19:40:05 +0300

    Initial Commit

---
+++ week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by Bilal on 24.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
